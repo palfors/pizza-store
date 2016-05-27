d8e90b2abeefc74740e2d0f83e4807346ef71890
Peter Alfors <palfors@gmail.com>
2016-05-27 16:45:59 -0500

    fixing Breadsticks case-sensitive issue

--- src/main/java/com/alforsconsulting/pizzastore/menu/sides/Breadsticks.java
+++ src/main/java/com/alforsconsulting/pizzastore/menu/sides/Breadsticks.java
@@ -1,7 +1,6 @@
 package com.alforsconsulting.pizzastore.menu.sides;
 
 import com.alforsconsulting.pizzastore.menu.GenericMenuItem;
-import com.alforsconsulting.pizzastore.menu.MenuItemIdGenerator;
 import com.alforsconsulting.pizzastore.menu.MenuItemType;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -9,16 +8,15 @@ import org.springframework.stereotype.Component;
 import javax.persistence.Entity;
 
 /**
- * Created by palfors on 5/8/16.
+ * Created by palfors on 5/27/16.
  */
 @Component
 @Scope("prototype")
 @Entity
 public class Breadsticks extends GenericMenuItem {
-
     public Breadsticks() {
         // default the menuItemType
-        this.setMenuItemType(MenuItemType.BREADSTICKS.getBeanName());
+        setMenuItemType(MenuItemType.BREADSTICKS.getBeanName());
     }
 
     public Breadsticks(long menuItemId) {
